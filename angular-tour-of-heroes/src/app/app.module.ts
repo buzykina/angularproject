8c01cc565eb8e5b9c7bfcf4a9db081df918d61bf
Buzykina,Yevheniia Y.V <y.buzykina@student.fontys.nl>
2018-09-08 11:44:03 +0200

    working version

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { TasksComponent } from './tasks/tasks.component';
-import { FormsModule } from '@angular/forms'; //Angular ngModel imported from here.
+import { FormsModule } from '@angular/forms';
+import { DepartmentsComponent } from './departments/departments.component'; //Angular ngModel imported from here.
 
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeesComponent,
-    TasksComponent
+    TasksComponent,
+    DepartmentsComponent
   ],
   imports: [
     BrowserModule,
