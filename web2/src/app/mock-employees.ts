8742213649644e094ed2d87545e1e5c66cb0c925
Rahul Raja Annadurai <rahulrannadurai@gmail.com>
2018-09-23 22:40:09 +0200

    Employees department id changed

--- web2/src/app/mock-employees.ts
+++ web2/src/app/mock-employees.ts
@@ -3,7 +3,7 @@ import { Employee } from './employee';
 export const Employees: Employee[] = [
     { 
       id: 1,
-      department_id: 3,
+      department_id: 1,
       first_name: 'Yevheniia',
         last_name: 'Buzykina',
         birth_date: "1999/12/29",
@@ -12,7 +12,7 @@ export const Employees: Employee[] = [
       },
       { 
       id: 2,
-      department_id: 4,
+      department_id: 2,
       first_name: 'Mihai',
         last_name: 'Jizdan',
         birth_date: "1999/03/12",
@@ -21,7 +21,7 @@ export const Employees: Employee[] = [
       },
       { 
       id: 3,
-      department_id: 5,
+      department_id: 2,
       first_name: 'Rahul',
         last_name: 'Annadurai',
         birth_date: "1999/08/29",
