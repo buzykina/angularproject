51aee50e6779347ef12769227db1d69fe9091975
Buzykina,Yevheniia Y.V <y.buzykina@student.fontys.nl>
2018-10-01 20:47:12 +0200

    searching works

--- web2/src/app/filter.pipe.ts
+++ web2/src/app/filter.pipe.ts
@@ -9,7 +9,15 @@ export class FilterPipe implements PipeTransform {
    	//check if search term is undefined
    	if (term === undefined) return tasks;
    	return tasks.filter(function(task) {
+   		if(task.hasOwnProperty('name'))
+   		{
    		return task.name.toLowerCase().includes(term.toLowerCase());
+   		}
+   		else if(task.hasOwnProperty('first_name'))
+   		{
+   		return task.first_name.toLowerCase().includes(term.toLowerCase());
+   		}
+
    	})
   }
 
